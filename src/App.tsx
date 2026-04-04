b8aed6fa6c293f3beb1ff358a52a3e5b5188c524
giseliodonto-dev <giseliodonto@gmail.com>
2026-04-04 17:22:05 -0300

    feat: reset estrutural para o VEGA 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { ProtectedRoute } from "@/components/ProtectedRoute";
 import Auth from "./pages/Auth";
 import Home from "./pages/Home";
-import Pacientes from "./pages/Pacientes";
-import Equipe from "./pages/Equipe";
-import Vendas from "./pages/Vendas";
-import Gestao from "./pages/Gestao";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -22,11 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/auth" element={<Auth />} />
-          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-          <Route path="/pacientes" element={<ProtectedRoute><Pacientes /></ProtectedRoute>} />
-          <Route path="/equipe" element={<ProtectedRoute><Equipe /></ProtectedRoute>} />
-          <Route path="/vendas" element={<ProtectedRoute><Vendas /></ProtectedRoute>} />
-          <Route path="/gestao" element={<ProtectedRoute><Gestao /></ProtectedRoute>} />
+          <Route path="/" element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
